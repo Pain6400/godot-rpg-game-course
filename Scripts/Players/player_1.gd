b602b2501e1ce8b30f19bd89d73da294b79ea20f
kevin_almendares <kevin_almendares@bantrab.com>
2025-09-14 11:20:59 -0600

    fix

--- Scripts/Players/player_1.gd
+++ Scripts/Players/player_1.gd
@@ -1,30 +1,74 @@
 extends CharacterBody3D
 
+@onready var _player_pcam: PhantomCamera3D = %PlayerPhantomCamera3D
+@onready var _aim_pcam: PhantomCamera3D = %PlayerAimPhantomCamera3D
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
-@onready var camera_3d: Camera3D = $"../../Camera3D"
+
+@onready var _player_direction: Node3D = %PlayerDirection
+
+@export var mouse_sensitivity: float = 0.05
+
+@export var min_pitch: float = -89.9
+@export var max_pitch: float = 50
+
+@export var min_yaw: float = 0
+@export var max_yaw: float = 360
+
+
+
+func _ready() -> void:
+	if _player_pcam.get_follow_mode() == _player_pcam.FollowMode.THIRD_PERSON:
+		Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
 
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
+	if velocity.length() > 0.2:
+		var look_direction: Vector2 = Vector2(velocity.z, velocity.x)
+		_player_direction.rotation.y = look_direction.angle()
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	#var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	var direction = camera_3d.global_transform.basis * Vector3(input_dir.x, 0, input_dir.y)
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
 
-	move_and_slide()
+func _unhandled_input(event: InputEvent) -> void:
+	if _player_pcam.get_follow_mode() == _player_pcam.FollowMode.THIRD_PERSON:
+		var active_pcam: PhantomCamera3D
+
+		_set_pcam_rotation(_player_pcam, event)
+		_set_pcam_rotation(_aim_pcam, event)
+		if _player_pcam.get_priority() > _aim_pcam.get_priority():
+			_toggle_aim_pcam(event)
+		else:
+			_toggle_aim_pcam(event)
+
+
+func _set_pcam_rotation(pcam: PhantomCamera3D, event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		var pcam_rotation_degrees: Vector3
+
+		# Assigns the current 3D rotation of the SpringArm3D node - so it starts off where it is in the editor
+		pcam_rotation_degrees = pcam.get_third_person_rotation_degrees()
+
+		# Change the X rotation
+		pcam_rotation_degrees.x -= event.relative.y * mouse_sensitivity
+
+		# Clamp the rotation in the X axis so it go over or under the target
+		pcam_rotation_degrees.x = clampf(pcam_rotation_degrees.x, min_pitch, max_pitch)
+
+		# Change the Y rotation value
+		pcam_rotation_degrees.y -= event.relative.x * mouse_sensitivity
+
+		# Sets the rotation to fully loop around its target, but witout going below or exceeding 0 and 360 degrees respectively
+		pcam_rotation_degrees.y = wrapf(pcam_rotation_degrees.y, min_yaw, max_yaw)
+
+		# Change the SpringArm3D node's rotation and rotate around its target
+		pcam.set_third_person_rotation_degrees(pcam_rotation_degrees)
+
+
+func _toggle_aim_pcam(event: InputEvent) -> void:
+	if event is InputEventMouseButton \
+		and event.is_pressed() \
+		and event.button_index == 2 \
+		and (_player_pcam.is_active() or _aim_pcam.is_active()):
+		if _player_pcam.get_priority() > _aim_pcam.get_priority():
+			_aim_pcam.set_priority(30)
+		else:
+			_aim_pcam.set_priority(0)
