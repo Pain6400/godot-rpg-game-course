7da8b54468ef5ca395029600b5b17075d2f76184
Kevin Almendares <kevin_almendares@bantrab.net.gt>
2025-09-19 11:38:20 -0600

    Revert "search"
    This reverts commit 7f99d7a63c730939c3a246d8ee053ea6e81297c1.

--- Scripts/Monsters/AI Melee/RunState.gd
+++ Scripts/Monsters/AI Melee/RunState.gd
@@ -1,36 +1,26 @@
 extends Node
 
 var AIController
-var run: bool = false
-var is_active: bool = true
+var run: bool
 
 func _ready() -> void:
 	AIController = get_parent().get_parent()
-	is_active = true
-	
-	# Si estamos atacando, esperar a que termine
 	if AIController.attacking:
-		await AIController.get_node("AnimationTree").animation_finished
+		await  AIController.get_node("AnimationTree").animation_finished
 		AIController.attacking = false
-	
-	# Si estamos despertando, esperar a que termine
-	if AIController.Awakening:
-		await AIController.get_node("AnimationTree").animation_finished
-		AIController.Awakening = false
+		
+	else:
+		run = false
+		AIController.get_node("AnimationTree").get("parameters/playback").travel("Awaken")
+		AIController.Awakening = true
+		await  AIController.get_node("AnimationTree").animation_finished
 	
 	run = true
+	AIController.Awakening = false
 	AIController.get_node("AnimationTree").get("parameters/playback").travel("Run")
 	
 func _physics_process(delta: float) -> void:
-	if AIController and run and is_active:
+	if AIController and run:
 		AIController.velocity.x = AIController.direction.x * AIController.speed
 		AIController.velocity.z = AIController.direction.z * AIController.speed
 		AIController.look_at(AIController.global_transform.origin + AIController.direction, Vector3(0,1,0))
-
-# Método para limpiar el estado
-func cleanup():
-	is_active = false
-	run = false
-
-func _exit_tree():
-	cleanup()
