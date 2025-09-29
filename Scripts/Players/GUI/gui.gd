c6917fc116b217e2a680171c0a9afdce70af2f0b
kevin_almendares <kevin_almendares@bantrab.com>
2025-09-28 20:43:29 -0600

    buttons

--- Scripts/Players/GUI/gui.gd
+++ Scripts/Players/GUI/gui.gd
@@ -1,5 +1,7 @@
 extends CanvasLayer
 @onready var containter: Control = $containter
+@onready var inventoty_button: Button = $containter/VBoxContainer/inventoty_button
+@onready var profile_button: Button = $containter/VBoxContainer/profile_button
 
 func  _ready() -> void:
 	containter.hide()
@@ -13,3 +15,11 @@ func _process(delta: float) -> void:
 				Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 			false:
 				Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+
+func _on_inventoty_button_pressed() -> void:
+	inventoty_button.disabled = true
+	profile_button.disabled = false
+
+func _on_profile_button_pressed() -> void:
+	pass # Replace with function body.
