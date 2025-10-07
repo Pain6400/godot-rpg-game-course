c2dc15cb27ada43fe5572b13fabffb6313093b24
Kevin Almendares <kevin_almendares@bantrab.net.gt>
2025-10-07 16:24:53 -0600

    inventory scrips

--- Scripts/Players/GUI/gui.gd
+++ Scripts/Players/GUI/gui.gd
@@ -2,6 +2,8 @@ extends CanvasLayer
 @onready var containter: Control = $containter
 @onready var inventoty_button: Button = $containter/VBoxContainer/inventoty_button
 @onready var profile_button: Button = $containter/VBoxContainer/profile_button
+@onready var inventory: Control = $containter/inventory
+@onready var profile: Control = $containter/profile
 
 func  _ready() -> void:
 	containter.hide()
@@ -20,6 +22,11 @@ func _process(delta: float) -> void:
 func _on_inventoty_button_pressed() -> void:
 	inventoty_button.disabled = true
 	profile_button.disabled = false
-
+	inventory.show()
+	profile.hide()
+	
 func _on_profile_button_pressed() -> void:
-	pass # Replace with function body.
+	inventoty_button.disabled = false
+	profile_button.disabled = true
+	inventory.hide()
+	profile.show()
